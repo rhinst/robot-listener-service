23ec055e63a91dae8aaff08fa3224cc88f60d1b7
Rob Hinst <robert.hinst@crowncastle.com>
2021-01-18 17:47:36 -0500

    Adds ability to select microphone sound card index and adds black formatting.

--- listener/__main__.py
+++ listener/__main__.py
@@ -16,25 +16,34 @@ MODELDIR = os.path.join(os.path.dirname(__file__), "../model")
 def main():
     environment: str = os.getenv("ENVIRONMENT", "dev")
     config: Dict = load_config(environment)
-    initialize_logger(level=config['logging']['level'], filename=config['logging']['filename'])
-    redis_host = config['redis']['host']
-    redis_port = config['redis']['port']
-    logger.debug(f"Connecting to redis at {redis_host}:{redis_port}")
-    redis_client: Redis = Redis(
-        host=redis_host, port=redis_port, db=0
+    initialize_logger(
+        level=config["logging"]["level"], filename=config["logging"]["filename"]
     )
+    redis_host = config["redis"]["host"]
+    redis_port = config["redis"]["port"]
+    logger.debug(f"Connecting to redis at {redis_host}:{redis_port}")
+    redis_client: Redis = Redis(host=redis_host, port=redis_port, db=0)
 
     logger.debug("Initializing PyAudio interface")
     audio = pyaudio.PyAudio()
     logger.debug("Intializing pocketsphinx Decoder")
     decoder_config = Decoder.default_config()
-    decoder_config.set_string('-hmm', os.path.join(MODELDIR, 'en-us/en-us'))
-    decoder_config.set_string('-lm', os.path.join(MODELDIR, 'en-us/en-us.lm.bin'))
-    decoder_config.set_string('-dict', os.path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
+    decoder_config.set_string("-hmm", os.path.join(MODELDIR, "en-us/en-us"))
+    decoder_config.set_string("-lm", os.path.join(MODELDIR, "en-us/en-us.lm.bin"))
+    decoder_config.set_string(
+        "-dict", os.path.join(MODELDIR, "en-us/cmudict-en-us.dict")
+    )
     decoder = Decoder(decoder_config)
 
     logger.debug("Opening audio stream")
-    stream = audio.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
+    stream = audio.open(
+        format=pyaudio.paInt16,
+        channels=1,
+        rate=16000,
+        input=True,
+        frames_per_buffer=1024,
+        input_device_index=config["microphone"]["card_index"],
+    )
     stream.start_stream()
 
     in_speech_bf = False
@@ -54,7 +63,9 @@ def main():
                         decoder.end_utt()
                         transcription = decoder.hyp().hypstr
                         logger.debug(f"Result: {transcription}")
-                        redis_client.publish("subsystem.listener.recording", transcription)
+                        redis_client.publish(
+                            "subsystem.listener.recording", transcription
+                        )
                         decoder.start_utt()
             else:
                 logger.debug("Buffer closed. Ending")
@@ -66,5 +77,5 @@ def main():
         redis_client.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
