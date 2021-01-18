1efa2178290c81dbc3da7c31b5e80426fd7f2849
Rob Hinst <robert.hinst@crowncastle.com>
2021-01-18 17:11:46 -0500

    Converted to use pocketsphinx and continuously listen and transcribe recognized speech

--- listener/__main__.py
+++ listener/__main__.py
@@ -1,36 +1,16 @@
 import os
 from itertools import cycle
-from time import sleep
 from typing import Dict
-from tempfile import NamedTemporaryFile
-import json
 
 from redis import Redis
-from redis.client import PubSub
-from speech_recognition import Microphone, Recognizer, AudioData, UnknownValueError, AudioSource
+import pyaudio
+from pocketsphinx.pocketsphinx import Decoder
 
 from listener.config import load_config
 from listener.logging import logger, initialize_logger
-from listener.microphone import get_microphone
 
 
-def on_record_timed(message: Dict, source: AudioSource, recognizer: Recognizer) -> AudioData:
-    duration = message['duration']
-    logger.debug(f"Recording {duration}s of audio")
-    return recognizer.record(source, duration=float(duration))
-
-
-def on_record_phrase(message, source: AudioSource, recognizer: Recognizer) -> AudioData:
-    logger.debug("Listening for a phrase")
-    return recognizer.listen(source, phrase_time_limit=10)
-
-
-def transcribe(audio: AudioData, recognizer: Recognizer) -> str:
-    try:
-        return recognizer.recognize_google(audio)
-    except UnknownValueError:
-        logger.warning("Unintelligible audio")
-        return ""
+MODELDIR = os.path.join(os.path.dirname(__file__), "../model")
 
 
 def main():
@@ -43,51 +23,46 @@ def main():
     redis_client: Redis = Redis(
         host=redis_host, port=redis_port, db=0
     )
-    pubsub: PubSub = redis_client.pubsub(ignore_subscribe_messages=True)
-    pubsub.subscribe("subsystem.listener.command")
-    handlers = {
-        "timed": on_record_timed,
-        "phrase": on_record_phrase
-    }
-    microphone_name = config['microphone']['name']
-    logger.debug(f"Listening on {microphone_name}")
-    microphone = get_microphone(microphone_name)
-    recognizer = Recognizer()
-    redis_client.publish("subsystem.listener.state", "idle")
+
+    logger.debug("Initializing PyAudio interface")
+    audio = pyaudio.PyAudio()
+    logger.debug("Intializing pocketsphinx Decoder")
+    decoder_config = Decoder.default_config()
+    decoder_config.set_string('-hmm', os.path.join(MODELDIR, 'en-us/en-us'))
+    decoder_config.set_string('-lm', os.path.join(MODELDIR, 'en-us/en-us.lm.bin'))
+    decoder_config.set_string('-dict', os.path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
+    decoder = Decoder(decoder_config)
+
+    logger.debug("Opening audio stream")
+    stream = audio.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
+    stream.start_stream()
+
+    in_speech_bf = False
+    decoder.start_utt()
+
     try:
+        logger.debug("Starting decoder loop")
         while cycle([True]):
-            # see if there is a command for me to execute
-            if redis_message := pubsub.get_message():
-                cmd_message = json.loads(redis_message['data'])
-                with microphone as source:
-                    redis_client.publish("subsystem.listener.state", "normalizing")
-                    logger.debug("Adjusting for ambient noise")
-                    recognizer.adjust_for_ambient_noise(source)
-                    redis_client.publish("subsystem.listener.state", "recording")
-                    audio = handlers[cmd_message['mode']](cmd_message, source, recognizer)
-                    redis_client.publish("subsystem.listener.state", "processing")
-                wav_data = audio.get_wav_data()
-                logger.debug(f"Captured {len(wav_data)} bytes of audio")
-                f = NamedTemporaryFile(delete=False, mode="wb")
-                f.write(audio.get_wav_data())
-                f.close()
-                request_id = cmd_message["request_id"]
-                transcription = transcribe(audio, recognizer)
-                out_message = {
-                    "request_id": request_id,
-                    "wav_file": f.name,
-                    "transcription": transcription
-                }
-                logger.debug(f"Fulfilled request ID #{request_id}")
-                if transcription:
-                    logger.debug(f"Transcription was '{transcription}'")
-                redis_client.publish("subsystem.listener.recording", json.dumps(out_message))
-                os.unlink(f.name)
-                redis_client.publish("subsystem.listener.state", "idle")
+            buf = stream.read(1024)
+            if buf:
+                logger.debug("Decoding raw audio")
+                decoder.process_raw(buf, False, False)
+                if decoder.get_in_speech() != in_speech_bf:
+                    logger.debug("GOT HERE")
+                    in_speech_bf = decoder.get_in_speech()
+                    if not in_speech_bf:
+                        decoder.end_utt()
+                        transcription = decoder.hyp().hypstr
+                        logger.debug(f"Result: {transcription}")
+                        redis_client.publish("subsystem.listener.recording", transcription)
+                        decoder.start_utt()
+            else:
+                logger.debug("Buffer closed. Ending")
+                break
+        decoder.end_utt()
     except Exception:
         logger.exception("Something bad happened")
     finally:
-        pubsub.close()
         redis_client.close()
 
 
